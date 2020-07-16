afe3215365879bc8f6fe5949c2601ec90db5607f
v.berthoud <v.berthoud@users.noreply.github.com>
2020-07-16 14:06:39 +0200

    Merge branch 'master' of https://github.com/grasshoff/philhistcomp

--- projects/piraha_language/CrawlPiraha.py
+++ projects/piraha_language/CrawlPiraha.py
@@ -10,21 +10,12 @@ ff = io.open(file_to_open, 'r', encoding='utf-8')
 nlp = spacy.load("en_core_web_sm")
 matcher = Matcher(nlp.vocab)
 
-patterns = [[{"LOWER": "two"}], [{"LOWER": "one"}], [{"LOWER": "is"}], [{"LOWER": "and"}]]
+patterns = [[{"LOWER": "two"}], [{"LOWER": "one"}], [{"LOWER": "is"}], [{"LOWER": "plus"}]]
 
 doc = nlp(ff.read())
 for pattern in patterns:
     print(pattern)
-    matcher.add("HelloWorld", None, pattern)
+    matcher.add("tempId", None, pattern)
     matches = matcher(doc)
     print(len(matches))
-    span = doc[matches[0][1]:matches[0][2]]
-    print(span.text)
-    #span = doc[matches[0].start:matches[0].end]
-    print("stop")
-    #span = doc[start:end]
-    '''
-    for match_id, start, end in matches:
-        string_id = nlp.vocab.strings[match_id]  # Get string representation
-        span = doc[start:end]  # The matched span
-        print(match_id, string_id, start, end, span.text)'''
+    matcher.remove("tempId")
